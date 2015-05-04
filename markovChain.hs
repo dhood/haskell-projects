c682da711540b9dcd0cc6e5c3778d4a984b57b8e
Deanna Hood <deanna.m.hood@gmail.com>
2015-05-04 15:19:48 -0400

    Allow named states instead of just integers

--- markovChain.hs
+++ markovChain.hs
@@ -1,8 +1,15 @@
 import Control.Monad.State.Lazy
 import qualified Data.Map as Map
 import qualified Numeric.LinearAlgebra.HMatrix as Mat
+import Data.List
 
-countTransitions :: [Int] -> State (Map.Map (Int, Int) Int) [Int]
+data TransitionMatrix t = 
+                   TransitionMatrix { mat :: Mat.Matrix Double, states :: [t] }
+instance (Show t) => Show (TransitionMatrix t) where 
+    show (TransitionMatrix mat states) = "States: " ++ show states ++ "\n" 
+                                        ++ show mat
+
+countTransitions :: (Eq t, Ord t) => [t] -> State (Map.Map (t, t) Int) [t]
 countTransitions [x] = return []
 countTransitions (x1:x2:xs) = do
     counts <- get
@@ -10,18 +17,22 @@ countTransitions (x1:x2:xs) = do
     r <- countTransitions (x2:xs)
     return (x1:r) -- used if runState or evalState is called, not execState
 
+incr_count :: (Ord t) => t -> Map.Map t Int -> Map.Map t Int
 incr_count x y = 
     Map.insert x (currVal+1) y
     where currVal = Map.findWithDefault 0 x y
 
-makeTransitionMatrix :: [Int] -> (Mat.Matrix Double) 
-makeTransitionMatrix l = convertToProbs $ (size Mat.>< size) $ makeDataList count size 
-    where count = execState (countTransitions l) Map.empty
-          size = maximum l
+makeTransitionMatrix :: (Eq t, Ord t) => [t] -> TransitionMatrix t 
+makeTransitionMatrix dataList = TransitionMatrix {
+                    mat = convertToProbs $ (size Mat.>< size) $ makeDataList count states 
+                    , states = states}
+    where count = execState (countTransitions dataList) Map.empty
+          states = nub dataList
+          size = length states
 
-makeDataList :: (Map.Map (Int, Int) Int) -> Int -> [Double]
-makeDataList m n = map (\x -> fromIntegral $ Map.findWithDefault 0 x m) indices
-    where indices = [(x, y) | x <- [1..n], y <- [1..n]]
+makeDataList :: (Eq t, Ord t) => (Map.Map (t, t) Int) -> [t] -> [Double]
+makeDataList transCount states = map (\x -> fromIntegral $ Map.findWithDefault 0 x transCount) indices
+    where indices = [(x, y) | x <- states, y <- states]
 
 convertToProbs :: Mat.Matrix Double -> Mat.Matrix Double
 convertToProbs mat = 
@@ -35,17 +46,18 @@ convertToProbs mat =
 predict :: Mat.Matrix Double -> Mat.Matrix Double -> Int -> Mat.Matrix Double
 predict transMat p_n 0 = p_n
 predict transMat p_n k = do
-    let p_nplusk = transMat Mat.<> predict transMat p_n (k-1)
-        rows = Mat.rows p_nplusk
-        sump = (1 Mat.>< rows) (replicate rows 1) Mat.<> p_nplusk 
+    let p_nplusk = (predict transMat p_n (k-1)) Mat.<> transMat
+        cols = Mat.cols p_nplusk
+        sump = p_nplusk Mat.<> (cols Mat.>< 1) (replicate cols 1)
         in p_nplusk / sump
 
-d = [1,1,1,1,1,2,2,2,2,2,2,2,5,6,5,6,5,6,5,6,5,6,5,6,5,6,5,1]
-k = 5
+--d = [1,1,1,1,1,2,2,2,2,2,2,2,5,6,5,6,5,6,5,6,5,6,5,6,5,6,5,1]
+d = words "rain shine rain shine clouds rain"
+k = 30 
 main = do 
     let transMat = makeTransitionMatrix d
-        p_0 = (6 Mat.>< 1) (1: replicate 5 0)
-        p_k = predict transMat p_0 k
-          in Mat.disp 3 p_k 
-
+        numStates = length (states transMat)
+        p_0 = (1 Mat.>< numStates) (1.0:replicate (numStates-1) 0.0)
+        p_k = predict (mat transMat) p_0 k
+          in Mat.disp 3 p_k --print transMat
 
